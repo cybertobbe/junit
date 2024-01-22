0aa6bf70947a621a2e75a1748e2d43b4c7de677b
Tobias Sjöström <143131053+cybertobbe@users.noreply.github.com>
2024-01-22 13:35:53 +0100

    Initial commit

---
+++ src/test/java/com/example/EmployeeTest.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+
+      @Test
+      @DisplayName("Check that getId returns correct Id")
+      void checkThatGetIdReturnsCorrectId() {
+            Employee employee = new Employee("1", 10000);
+            var result = employee.getId();
+            assertThat(result).isEqualTo("1");
+      }
+
+      @Test
+      @DisplayName("Check that setId sets right id")
+      void checkThatSetIdSetsRightId() {
+            Employee employee = new Employee("2", 10000);
+            employee.setId("2");
+            var result = employee.getId();
+            assertThat(result).isEqualTo("2");
+      }
+
+      @Test
+      @DisplayName("Check that setSalary sets correct salary")
+      void checkThatSetSalarySetsCorrectSalary() {
+            Employee employee = new Employee("3", 10000);
+            employee.setSalary(10000);
+            var result = employee.getSalary();
+            assertThat(result).isEqualTo(10000);
+      }
+
+
+
+
+
+}
