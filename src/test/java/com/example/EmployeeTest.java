caf361237ea23b516d657cc4f14c88ce7dd76a50
Tobias Sjöström <143131053+cybertobbe@users.noreply.github.com>
2024-01-22 19:20:11 +0100

    Addiing tests to boolean methods, both true and false

--- src/test/java/com/example/EmployeeTest.java
+++ src/test/java/com/example/EmployeeTest.java
@@ -1,11 +1,10 @@
 package com.example;
 
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class EmployeeTest {
 
