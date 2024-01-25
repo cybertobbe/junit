3c20d06203691083dfc5dad8cb0f12c2c45dc317
Tobias Sjöström <143131053+cybertobbe@users.noreply.github.com>
2024-01-25 17:25:41 +0100

    Update files and pom.xml

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -21,34 +21,30 @@ class EmployeesTest {
             EmployeeRepository employeeRepository = new EmployeeRepositoryStub();
             Employees employees = new Employees(employeeRepository, bankService);
             Employee employee = new Employee("1", 1000);
-            int payments = employees.payEmployees();
+            int numberOfPayments = employees.payEmployees();
 
-            assertThat(payments).isEqualTo(2);
+            assertThat(numberOfPayments).isEqualTo(2);
 
       }
 
       @Test
       @DisplayName("If RuntimeException is thrown then payments should be 0")
       void ifRuntimeExceptionIsThrownThenPaymentsShouldBe0() {
-            BankService bankService = Mockito.mock(BankService.class);
-            EmployeeRepository employeeRepository = Mockito.mock(EmployeeRepository.class);
-            Employees employees = new Employees(employeeRepository, bankService);
-
-            Employee employee = new Employee("1", 1000);
-            List<Employee> employeeList = Arrays.asList(employee);
-            when(employeeRepository.findAll()).thenReturn(employeeList);
-            doThrow(RuntimeException.class).when(bankService).pay(anyString(), anyDouble());
-            verify(bankService, times(0)).pay(anyString(), anyDouble());
-
+            BankService bankServiceMock = Mockito.mock(BankService.class);
+            doThrow(RuntimeException.class).when(bankServiceMock).pay(anyString(), anyDouble());
+            EmployeeRepository employeeRepository = new EmployeeRepositoryStub();
+            Employees employees = new Employees(employeeRepository, bankServiceMock);
             // Act
             int payments = employees.payEmployees();
-
             // Assert
             assertThat(payments).isEqualTo(0);
-
-
-
+            verify(bankServiceMock, times(2)).pay(anyString(), anyDouble());
+            assertThat(employeeRepository.findAll()).extracting(Employee::isPaid).containsOnly(false);
       }
+
+
+
+
 
 
 
